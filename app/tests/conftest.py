a24e77e980cf91338a390b4411644fce7c701f5e
stephanmundtcode <stephan.mundt@code.berlin>
2026-03-30 21:26:20 +0200

    Implemented short memory database for testing

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from app.app import create_app
+from os import environ
 
 @pytest.fixture
 def client():
+    environ['DATABASE_URL'] = 'sqlite://'
     app = create_app()
 
     with app.app_context():
