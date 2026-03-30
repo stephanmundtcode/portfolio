37af66fcd4d9d934b49a6f55f5c282bbef8ce332
stephanmundtcode <stephan.mundt@code.berlin>
2026-03-30 21:21:59 +0200

    finished SQLAlchemy setup for sqlite

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
+from nt import environ
 from dotenv import load_dotenv
+from os import environ
 
+# Load environment variables from the .env file
 load_dotenv()
+
+# Access environment variables as if they came from the actual environment
+SQLALCHEMY_DATABASE_URI=environ.get('DATABASE_URL')
