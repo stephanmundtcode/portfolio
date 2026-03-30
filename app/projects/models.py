d0af7b553f0adb54b804e41507dd70e71f37b1ed
stephanmundtcode <stephan.mundt@code.berlin>
2026-03-31 00:59:21 +0200

    created many to many connection (including merging table) for Project and Tag

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,15 +1,27 @@
 import uuid
-from . import routes, models
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.extensions.database import db
-from sqlalchemy import UUID
+from sqlalchemy import UUID, Column, ForeignKey
+
+# merging table for Project and Tag
+project_tag = db.Table("project_tag", 
+    Column("project_id", ForeignKey("project.id"), primary_key=True),
+    Column("tag_id", ForeignKey("tag.id"), primary_key=True)
+)
 
 class Project(db.Model):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     slug: Mapped[str] = mapped_column(unique=True)
     title: Mapped[str] = mapped_column(unique=True)
     description: Mapped[str]
-    tags: Mapped[str]
+    tags: Mapped[list["Tag"]] = relationship(secondary=project_tag, back_populates="projects")
     link: Mapped[str]
     year: Mapped[int]
     picture_url: Mapped[str] = mapped_column(nullable=True)
+
+# n to n relationship between tags and Projects
+
+class Tag(db.Model):
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    tag: Mapped[str] = mapped_column(unique=True)
+    projects: Mapped[list["Project"]] = relationship(secondary=project_tag, back_populates="tags")
