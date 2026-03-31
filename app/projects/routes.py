c939745043f5d79b6095bc53237098bd3359ff80
stephanmundtcode <stephan.mundt@code.berlin>
2026-03-31 03:28:52 +0200

    got rid of json and showed database content on projects page

--- app/projects/routes.py
+++ app/projects/routes.py
@@ -1,8 +1,10 @@
+# database
+from sqlalchemy import select
+from app.extensions.database import db
+
 from .models import Project
 from flask import Blueprint, render_template
 import json
-
-PROJECT_DATA_PATH: str = "app/mock_data/projects.json"
 
 def load_json(file) -> list[dict] | str:
      try:
@@ -16,18 +18,17 @@ blueprint = Blueprint('projects', __name__)
 
 @blueprint.get('/projects')
 def projects() -> str:
-    projects = load_json(PROJECT_DATA_PATH)
-    for project in projects:
-        project["tags"] = " | ".join(project["tags"])
-
+    stmt = select(Project).order_by(Project.year)
+    projects = db.session.scalars(stmt).all()
     return render_template('projects/index.html', projects=projects)
 
 
 @blueprint.get('/projects/<slug>')
 def project(slug) -> str:
-    projects = load_json(PROJECT_DATA_PATH)
+    stmt = select(Project)
+    projects = db.session.scalars(stmt).all()
     for project in projects:
-        if slug == project["slug"]:
+        if slug == project.slug:
             return render_template('projects/single_project.html', project=project)
     
     return "Sorry, unfortunately there is no project like that :(", 404
