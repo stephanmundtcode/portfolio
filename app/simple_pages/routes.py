bdb0cedf3b39db53310862f3bb2634e11d21f16b
stephanmundtcode <stephan.mundt@code.berlin>
2026-04-10 21:15:21 +0200

    implemented upload of contact form into database

--- app/simple_pages/routes.py
+++ app/simple_pages/routes.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
+from datetime import datetime
+from flask import Blueprint, render_template, redirect, url_for, request
+from app.extensions.database import db
+from app.simple_pages.models import Contact
+
 
 blueprint = Blueprint('simple_pages', __name__)
 
@@ -10,6 +14,16 @@ def index() -> str:
 def index_redirect() -> str:
     return redirect(url_for('simple_pages.index'))
 
-@blueprint.get('/contact')
+@blueprint.route('/contact' , methods=['GET', 'POST'])
 def contact() -> str:
-        return render_template('simple_pages/contact.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        date = datetime.utcnow()
+
+        new_entry = Contact(name=name, email=email, message=message, created_at=date)
+        db.session.add(new_entry)
+        db.session.commit()
+        
+    return render_template('simple_pages/contact.html')
